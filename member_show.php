a6d03b9eff03a5fa56f5f6206c2d1a85e52ad687
insipthus <6421061015@cdti.ac.th>
2023-07-05 15:17:39 +0700

    update

---
+++ member_show.php
@@ -0,0 +1,18 @@
+<?php
+include "connect.php";
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "$row[firstname] $row[lastname] $row[email] $row[phone] $row[address] <br/>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
---
+++ product_show.php
@@ -0,0 +1,18 @@
+<?php
+include "connect.php";
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    echo "$row[name] $row[prize] $row[stock] $row[description]  <br/>";
+  }
+} else {
+  echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
