fc660696950c3fbcd20c1c30b7957f8230072a36
insipthus <6421061015@cdti.ac.th>
2023-07-26 11:34:01 +0700

    update

--- member_edit.php
+++ member_edit.php
@@ -6,6 +6,8 @@ $sql ="SELECT * FROM tbl_member WHERE id='$_GET[id]'";
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result)
 ?>
+<div class="container">
+    <h1 class ="text-center">แก้ไขสมาชิก</h1>
 <form action="member_update.php?id=<?php echo $row[id] ?>" method="post" enctype="multipart/form-data">
   ชื่อ<input type="text" name="firstname" value="<?php echo $row[firstname]; ?>"class="input"/>
   นามสกุล<input type="text" name="lastname" value="<?php echo $row[lastname]; ?>"class="input"/>
